447f12fac681b7ba971ea722fed8c6b2889d606f
Robert Orsulics <robert.orsulics@seon.io>
2024-05-30 11:02:02 +0200

    DEF-13 Remove unused async keyword

--- lib/seon_sdk_flutter_plugin.dart
+++ lib/seon_sdk_flutter_plugin.dart
@@ -5,7 +5,7 @@ class SeonSdkFlutterPlugin {
     return SeonSdkFlutterPluginPlatform.instance.getFingerprint(sessionId);
   }
 
-  void setGeolocationEnabled(bool enabled) async {
+  void setGeolocationEnabled(bool enabled) {
     return SeonSdkFlutterPluginPlatform.instance.setGeolocationEnabled(enabled);
   }
 
