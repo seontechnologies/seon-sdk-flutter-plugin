5ac64fce8138b3a91e24737266479e831c9670f9
Tamas Szalay <tamas.szalay@seon.io>
2026-05-28 15:56:36 +0200

    DEF-3714 remove todos from Package.swift

--- ios/seon_sdk_flutter_plugin/Package.swift
+++ ios/seon_sdk_flutter_plugin/Package.swift
@@ -6,14 +6,9 @@ import PackageDescription
 let package = Package(
     name: "seon_sdk_flutter_plugin",
     platforms: [
-        // TODO: Update the platforms your plugin supports.
-        // If your plugin only supports iOS, remove `.macOS(...)`.
-        // If your plugin only supports macOS, remove `.iOS(...)`.
         .iOS("15.0"),
     ],
     products: [
-        // TODO: Update your library and target names.
-        // If the plugin name contains "_", replace with "-" for the library name.
         .library(name: "seon-sdk-flutter-plugin", targets: ["seon_sdk_flutter_plugin"])
     ],
     dependencies: [
@@ -25,23 +20,12 @@ let package = Package(
     ],
     targets: [
         .target(
-            // TODO: Update your target name.
             name: "seon_sdk_flutter_plugin",
             dependencies: [
                 .product(name: "FlutterFramework", package: "FlutterFramework"),
                 .product(name: "SeonSDK", package: "seon-ios-sdk-swift-package"),
             ],
             resources: [
-                // TODO: If your plugin requires a privacy manifest
-                // (e.g. if it uses any required reason APIs), update the PrivacyInfo.xcprivacy file
-                // to describe your plugin's privacy impact, and then uncomment this line.
-                // For more information, see:
-                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
-
-                // TODO: If you have other resources that need to be bundled with your plugin, refer to
-                // the following instructions to add them:
-                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
             ]
         )
     ]
